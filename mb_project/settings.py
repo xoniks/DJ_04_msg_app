6cb904706b067e579489f459a47a1d80f45e69c0
xonidjango <110489575+xonidjango@users.noreply.github.com>
2022-08-13 12:54:55 +0200

    deploy

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-p4n%!q8@mi)-&*#gp44%20d5909jjww+c3ez*ll$%l8j1_31zq"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 from .models import Post
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
     model = Post
-    template = 'home.html'
+    template_name = 'home.html'
+    context_object_name = 'all_posts_list'
     
